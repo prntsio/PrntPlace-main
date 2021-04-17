20640b4990fc4902c35ac0771ce491bf69f6a763
dishaagarwalla <dishaagarwalla2001@gmail.com>
2021-04-17 12:07:06 +0530

    Disconnect fixed | WalletLink

--- client/src/components/Artist/Create.js
+++ client/src/components/Artist/Create.js
@@ -158,7 +158,7 @@ const Create = ({account}) => {
                         <div className="upload-file">
                             <div className="upload-text">
                                 <h3>Upload Thumbnail</h3>
-                                <p>Suggested image size: 302px x 280px</p>
+                                <p>We recommend image size to be 302px x 280px.</p>
                             </div>
                             <div className="choose-file">
                                 <form onSubmit={pinImageToIPFS}>
